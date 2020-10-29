0775995efea4726d139c62fcdac716925b853afb
ilovesemicolons <ilovesemicolons@hotmail.com>
2020-10-30 00:11:03 +0100

    .

--- src/home.js
+++ src/home.js
@@ -25,7 +25,7 @@ export default class Home extends React.Component {
 
     callProjectTopicAPI() {
 
-        fetch("http://localhost:9000/homePageProjectTopic")
+        fetch("http://142.93.39.216/homePageProjectTopic")
             .then(response => response.json())
             .then(response => this.setState({projectTopicAPIResponse: response}))
             .catch(function () {
