380ce7f92b6793bebe2994b992dce9d8436e9635
iLoveSemicolons <ilovesemicolons@hotmail.com>
2021-04-15 00:13:55 +0200

    IP

--- src/components/globalIP.js
+++ src/components/globalIP.js
@@ -3,7 +3,9 @@
 
 let address = {
   developmentAddress : "//localhost:5000",
-  productionAddress : "//api-dot-ilovesemicolons.ew.r.appspot.com"
+  // productionAddress : "//api-dot-ilovesemicolons.ew.r.appspot.com"
+  productionAddress : "//ilovesemicolons.io:9000"
+  
 };
 
 
